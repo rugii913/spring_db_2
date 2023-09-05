9c56834f7b5f76b0f18caa46c682ad898a158168
rugii913 <rugii913@gmail.com>
2023-09-05 18:11:56 +0900

    테스트-@Transactional, @Commit @Rollback
    *PlatformTransactionManager를 직접 사용하지 않고, 편리하게 다른 테스트와의 격리, 반복 실행 가능

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,23 +5,24 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional // 원래는 트랜잭션 시작하고 문제 없으면 커밋하는 어노테이션 - 테스트에서는 기본이 항상 롤백
+@Commit // @Rollback(value = false)
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+    /*
     // 트랜잭션 관련 코드
     @Autowired
     PlatformTransactionManager transactionManager; // cf. 스프링 부트는 트랜잭션 매니저 자동 등록 대상
@@ -33,6 +34,7 @@ class ItemRepositoryTest {
         // 트랜잭션 시작
         status = transactionManager.getTransaction(new DefaultTransactionDefinition());
     }
+    */
 
     @AfterEach
     void afterEach() {
@@ -40,8 +42,10 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        /*
         // 트랜잭션 롤백
         transactionManager.rollback(status);
+        */
     }
 
     @Test
