8bad009e7f8f7842f78e037af1c572aaaf7592b3
rugii913 <rugii913@gmail.com>
2023-09-19 17:36:23 +0900

    트랜잭션 AOP 주의사항-프록시 내부 호출 2 + 접근제어자 관련
    *** 프록시 방식 AOP 한계 때문에 내부 호출에 프록시를 적용할 수 없는 문제
    => 간단하고 자주 사용되는 대응책: AOP 로직이 적용되어야하는 메서드를 별도 클래스로 분리 => 내부 호출을 외부 호출로 바꾼다.
    => 복잡한 방법: AspectJ 사용하여, 프록시 방식이 아닌 컴파일 혹은 클래스 로딩 시점에 AOP 코드 넣기
    
    *** 스프링의 트랜잭션 AOP 기능은 public 메서드에만 트랜잭션을 적용하도록
    기본 설정이 되어있음에 유의
    - cf. 스프링 부트 3.0부터는 protected, package-visible에도 트랜잭션 적용됨

---
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV2Test.java
@@ -0,0 +1,77 @@
+package hello.springtx.apply;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV2Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass()); // CallService에는 아예 프록시도 적용 안 되어 있음
+    }
+
+    @Test
+    void externalCallV2() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService(internalService());
+        }
+
+        @Bean
+        InternalService internalService() {
+            return new InternalService();
+        }
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class CallService {
+
+        private final InternalService internalService;
+
+        public void external() { // 트랜잭션이 없는 메서드
+            log.info("call external");
+            printTxInfo();
+            internalService.internal();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+
+    static class InternalService {
+
+        @Transactional
+        public void internal() { // 트랜잭션이 적용되길 의도한 메서드
+        // void internal() { // cf. 스프링의 트랜잭션 AOP 기능은 public 메서드에만 트랜잭션을 적용하도록 기본 설정 되어있음
+            // cf. 스프링 부트 3.0부터는 protected, package-visible에도 트랜잭션 적용
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
