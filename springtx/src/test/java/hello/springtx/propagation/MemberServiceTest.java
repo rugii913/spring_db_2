48288012e500d6405ca18ca7486e720cdff3eca3
rugii913 <rugii913@gmail.com>
2023-10-15 18:30:03 +0900

    전파 활용2-서비스 계층에 트랜잭션이 없는 경우의 롤백

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,12 +1,12 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @SpringBootTest
@@ -37,5 +37,23 @@ class MemberServiceTest {
         // Then: 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService     @Transactional: OFF
+     * memberRepository  @Transactional: ON
+     * logRepository     @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        // Given
+        String username = "로그예외_outerTxOff_fail";
+
+        // When: logRepository의 트랜잭션에서 예외 발생 및 롤백
+        assertThatThrownBy(() -> memberService.joinV1(username)).isExactlyInstanceOf(RuntimeException.class);
+
+        // Then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다. - 데이터 정합성에 문제가 생김
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
